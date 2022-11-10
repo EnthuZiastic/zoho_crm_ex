fe1375b082052948ece66e876442f0f2b7c09fa4
aayushthapa <aayushthapa7@gmail.com>
2022-11-10 22:10:23 +0545

    api input request module

---
+++ lib/zoho_crm/input_request.ex
@@ -0,0 +1,23 @@
+defmodule ZohoCrm.InputRequest do
+  @moduledoc """
+  Api input request data structure
+  """
+  defstruct [:module_api_name, :body, :query_params, :access_token]
+
+  @enforce_keys [:access_token]
+  @type t() :: %__MODULE__{
+          access_token: String.t(),
+          module_api_name: String.t() | nil,
+          query_params: map(),
+          body: map()
+        }
+
+  def new(access_token, module_api_name \\ nil, query_params \\ %{}, body \\ %{}) do
+    %__MODULE__{
+      access_token: access_token,
+      body: body,
+      module_api_name: module_api_name,
+      query_params: query_params
+    }
+  end
+end
