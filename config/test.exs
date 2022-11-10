2c15fb14928252e21eb266ffdf5b879106bf859b
aayushthapa <aayushthapa7@gmail.com>
2022-11-10 21:34:05 +0545

    set refresh token env variable

--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,5 @@ use Mix.Config
 
 config :zoho_crm, :zoho,
   client_id: {:system, "ZOHO_CLIENT_ID"},
-  client_secret: {:system, "ZOHO_CLIENT_SECRET"}
+  client_secret: {:system, "ZOHO_CLIENT_SECRET"},
+  refresh_token: {:system, "ZOHO_REFRESH_TOKEN"}
