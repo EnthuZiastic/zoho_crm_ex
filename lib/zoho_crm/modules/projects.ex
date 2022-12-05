2d84455ec9327dd5acc0567dff1e06012b1ba513
aayushthapa <aayushthapa7@gmail.com>
2022-12-05 17:54:43 +0545

    delete comments api function

--- lib/zoho_crm/modules/projects.ex
+++ lib/zoho_crm/modules/projects.ex
@@ -12,6 +12,7 @@ defmodule ZohoCrm.Modules.Projects do
   @type portal_id() :: String.t()
   @type project_id() :: String.t()
   @type task_id() :: String.t()
+  @type comment_id() :: String.t()
 
   @spec list_tasks(ZohoCrm.InputRequest.t(), portal_id(), project_id()) ::
           {:error, any} | {:ok, any}
@@ -80,6 +81,48 @@ defmodule ZohoCrm.Modules.Projects do
     |> Request.set_base_url(@project_base)
     |> Request.with_path(path)
     |> Request.with_method(:post)
+    |> Request.with_body(r.body)
+    |> Request.with_params(r.query_params)
+    |> Request.set_headers(r.access_token)
+    |> Request.send()
+  end
+
+  @spec update_comment(
+          ZohoCrm.InputRequest.t(),
+          portal_id(),
+          project_id(),
+          task_id(),
+          comment_id()
+        ) ::
+          {:error, any} | {:ok, any}
+  def update_comment(%InputRequest{} = r, portal_id, project_id, task_id, comment_id) do
+    path = "/portal/#{portal_id}/projects/#{project_id}/tasks/#{task_id}/comments/#{comment_id}"
+
+    Request.new(@api_type)
+    |> Request.set_base_url(@project_base)
+    |> Request.with_path(path)
+    |> Request.with_method(:post)
+    |> Request.with_body(r.body)
+    |> Request.with_params(r.query_params)
+    |> Request.set_headers(r.access_token)
+    |> Request.send()
+  end
+
+  @spec delete_comment(
+          ZohoCrm.InputRequest.t(),
+          portal_id(),
+          project_id(),
+          task_id(),
+          comment_id()
+        ) ::
+          {:error, any} | {:ok, any}
+  def delete_comment(%InputRequest{} = r, portal_id, project_id, task_id, comment_id) do
+    path = "/portal/#{portal_id}/projects/#{project_id}/tasks/#{task_id}/comments/#{comment_id}"
+
+    Request.new(@api_type)
+    |> Request.set_base_url(@project_base)
+    |> Request.with_path(path)
+    |> Request.with_method(:delete)
     |> Request.with_body(r.body)
     |> Request.with_params(r.query_params)
     |> Request.set_headers(r.access_token)
