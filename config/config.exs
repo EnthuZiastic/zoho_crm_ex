a57055014646792af5e709012d541db3aa1672ce
aayushthapa <aayushthapa7@gmail.com>
2022-12-07 14:55:43 +0545

    fix code

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ use Mix.Config
 config :zoho_crm, :zoho,
   client_id: {:system, "ZOHO_CLIENT_ID"},
   client_secret: {:system, "ZOHO_CLIENT_SECRET"},
-  refresh_token: {:system, "ZOHO_REFRESH_TOKEN"}
+  refresh_token: {:system, "ZOHO_REFRESH_TOKEN"},
   project_refresh_token: {:system, "ZOHO_PROJECT_REFRESH_TOKEN"}
 
 # It is also possible to import configuration files, relative to this
