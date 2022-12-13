affb1668b1695f5e2a9a678a1f56afc3a1385413
aayushthapa <aayushthapa7@gmail.com>
2022-12-13 16:54:17 +0545

    get recruit function

---
+++ lib/zoho_crm/modules/recruit/records.ex
@@ -0,0 +1,18 @@
+defmodule ZohoCrm.Modules.Recruit.Records do
+  alias ZohoCrm.Request
+  alias ZohoCrm.InputRequest
+
+  @api_type "recruit"
+  @version "v2"
+
+  def get_recruit_records(%InputRequest{} = r) do
+    Request.new(@api_type)
+    |> Request.with_version(@version)
+    |> Request.with_path("#{r.module_api_name}")
+    |> Request.with_method(:get)
+    |> Request.with_params(r.query_params)
+    |> Request.with_body(%{"data" => r.body})
+    |> Request.set_headers(r.access_token)
+    |> Request.send()
+  end
+end
