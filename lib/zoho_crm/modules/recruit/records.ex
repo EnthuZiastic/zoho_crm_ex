1e67ab428cac47485f41a01420c53acfd260fded
aayushthapa <aayushthapa7@gmail.com>
2022-12-13 16:54:43 +0545

    insert recruit record

--- lib/zoho_crm/modules/recruit/records.ex
+++ lib/zoho_crm/modules/recruit/records.ex
@@ -15,4 +15,15 @@ defmodule ZohoCrm.Modules.Recruit.Records do
     |> Request.set_headers(r.access_token)
     |> Request.send()
   end
+
+  def insert_recruit_records(%InputRequest{} = r) do
+    Request.new(@api_type)
+    |> Request.with_version(@version)
+    |> Request.with_path("#{r.module_api_name}")
+    |> Request.with_method(:post)
+    |> Request.with_params(r.query_params)
+    |> Request.with_body(%{"data" => r.body})
+    |> Request.set_headers(r.access_token)
+    |> Request.send()
+  end
 end
